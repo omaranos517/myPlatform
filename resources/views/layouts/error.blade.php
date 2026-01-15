a10263b0a45611ebca5fe076dc7d6910013e39c9
Omar Anos <omaranos517@gmail.com>
2026-01-15 14:16:05 +0200

    i made error.css imports shared.css instead of using vite

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -7,10 +7,7 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    @vite([
-        'resources/css/shared.css',
-        'resources/css/errors.css',
-    ])
+    @vite( 'resources/css/errors.css' )
 </head>
 <body>
 <main class="main-content">
